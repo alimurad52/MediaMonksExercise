91ed5e76e6d3882d1617af740cad63bb1c5eebc4
alimurad52 <alimurad52@hotmail.com>
2018-08-23 09:46:09 +0800

    Initial Commit

---
+++ MediaMonksExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MediaMonksExercise
+//
+//  Created by Ali Murad on 23/08/2018.
+//  Copyright © 2018 Media Monks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
